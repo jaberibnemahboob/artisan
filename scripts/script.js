133e7cdd71bc229455e898eb02b2da15cd87581c
Tbjorkman <tabithabjorkman@gmail.com>
2017-12-05 12:31:39 +0100

    Some of the css

---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+
+
+let menu = document.querySelector('#offCanvasNav');
+let timesX = document.querySelector('#times_nav_header');
+let line1 = document.querySelector('#line_1');
+let line2 = document.querySelector('#line_2');
+let line3 = document.querySelector('#line_3');
+let subCatMenu = document.querySelector('#subCategoryMenu');
+let subMenuArt = document.querySelector('#subCategoryArt');
+let subCaretCat = document.querySelector('#subCategoryCaret');
+let subCaretArt = document.querySelector('#subCategoryArtCaret');
+
+//myLink.addEventListener('click', openMenu);
+//USE WHEN READY TO TEST
+//myLink.addEventListener('touchend', rotateCarrot);
+
+//document.onload(menuSlide());
+
+/*
+function menuSlide() {
+
+   caret.classList.add('slide');
+}
+*/
+
+
+function toggleMenu() {
+    menu.classList.toggle('displayBlock');
+    timesX.classList.toggle('hidden');
+    line1.classList.toggle('hidden');
+    line2.classList.toggle('hidden');
+    line3.classList.toggle('hidden');
+}
+
+
+function toggleSubMenu() {
+    subCatMenu.classList.toggle('displayBlock');
+    subCaretCat.classList.toggle('arrow_carrot');
+}
+
+function toggleSubMenuArt() {
+    subMenuArt.classList.toggle('displayBlock');
+    subCaretArt.classList.toggle('arrow_carrot');
+}

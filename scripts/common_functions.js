b16bead35547fee562f22a4a8afdc07e94453f59
Jaber Ibne Mahboob <jaber.ibne.mahboob@gmail.com>
2017-12-06 13:58:56 +0100

    parallex effect

---
+++ scripts/common_functions.js
@@ -0,0 +1,3 @@
+function bodyHeight(){
+    return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight )
+}
